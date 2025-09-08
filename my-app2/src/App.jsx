0353341b252e51f110d820c99ddd3d0fd30ec7cd
rupaliwakhare <you@example.com>
2025-09-08 11:06:01 +0530

    theme

--- my-app2/src/App.jsx
+++ my-app2/src/App.jsx
@@ -1,15 +1,12 @@
-// src/App.jsx
-// import ThemeProvider from "./component/hooks/contextApi/theme/ThemeProvider";
-// import Navbar from "./component/hooks/contextApi/theme/Navbar";
-// import Prob1 from "./component/hooks/useRef/Prob1"
 
-import Allroutes from "./component/Allroutes";
-import Counter from "./component/props/Counter";
-import Navbar from "./component/Routing/Navbar";
+import Navbar from "./component/theme/Navbar";
+import ThemeProvider from "./component/theme/ThemeProvider";
 
 function App() {
   return (
-    <Allroutes/>
+  <ThemeProvider>
+    <Navbar/>
+  </ThemeProvider>
     
   );
 }
--- my-app2/src/component/theme/Navbar.jsx
+++ my-app2/src/component/theme/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+
+import React,{useContext} from 'react'
+import ThemeContext from './ThemeContext'
 
 const Navbar = () => {
+  const [theme,toggleTheme] = useContext(ThemeContext);
   return (
-    <div>
-      
-    </div>
-  )
-}
+   <nav 
+   style={{
+    background:theme === "light" ? "#fff" :"#000",
+    color:theme === "light" ? "#000" : "#fff"
+   }}>
+    <h1>
+      Hello
+    </h1>
+    <button onClick={toggleTheme}>
+      Switch {theme === "light" ? "Dark" :"light"}</button>
+   </nav>
+  );
+};
 
 export default Navbar
